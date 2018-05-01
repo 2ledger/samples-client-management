a36d186c896a53c54a63e4b5bb7c022bb3c5375f
Marcio Alexandre Macedo Rodrigues <marcioamr@MacBook-Pro-de-Marcio.local>
2018-05-01 11:41:28 -0300

    Initial adjustments

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ var path = require('path');
 global.pathRootApp = path.resolve(__dirname);
 global.varsAmbiente = require(pathRootApp + '/var.properties.js');
 
-var logger = require('./lib/logger.js');
-
 app.use(methodOverride());
 app.use(bodyParser.json({limit:1024102420, type:'application/json'}));
 app.use(bodyParser.text());
@@ -38,12 +36,11 @@ app.use('/', express.static(__dirname + '/public/', { 'index': 'index.html' }));
  
 app.listen(cfg.httpPort, cfg.httpsHost, function () {
     console.info("########################################################################");
-    console.info("##              POWER        SERVIDOR INICIADO           POWER        ##");
+    console.info("##              POWER        SERVER STARTED              POWER        ##");
     console.info("########################################################################");
-    console.info('Ambiente: ', cfg.env);
+    console.info('Enviroment: ', cfg.env);
     console.info('URL: ', cfg.httpHost + ":" + cfg.httpPort);
     console.info("------------------------------------------------------------------------");
-    logger.info("Servidor iniciado na porta:", cfg.httpHost + ":" + cfg.httpPort);
 });
 
 console.info("------------------------------------------------------------------------");
