7177c3196a5c3c2195e272a3dcb9de7bf252bd86
ESCRITORIO\Marcelo Beltrão <marcelo.beltrao@outlook.com>
2018-05-02 07:27:15 -0300

    Adjusting Login page

--- public/css/graficos.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app) {
-	var graficos = app.controllers.graficosController;
-		
-	app.post('/chat/buscasAtendimentosPorMes', graficos.buscasAtendimentosPorMes);
-	app.post('/chat/buscasAtendimentosPorAno', graficos.buscasAtendimentosPorAno);	
-	app.post('/chat/buscasAtendimentosPorDia', graficos.buscasAtendimentosPorDia);	
-	app.post('/chat/buscasAtendimentosPorCooperativa', graficos.buscasAtendimentosPorCooperativa);	
-	
-};
