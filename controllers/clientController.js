390617973131a9e76122735de6e46d02ccd6a0d1
2Ledger <38800594+2ledger@users.noreply.github.com>
2018-05-26 17:07:46 -0300

    Update clientController.js

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -25,7 +25,7 @@ console.log('(3):' + global.cfg);
 				path: '/v1' + path,
 				headers: {
 					'Content-Type': 'application/json',
-					'X-JWT-Assertion': auth
+					'X-JWT-Assertion': global.cfg.API_2LEDGER_TOKEN
 				}
 			};
 			
